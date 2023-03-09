0d8b9360b17c12d9cae0c2b5ee973ee462e371aa
louis0320 <zldqjrj@naver.com>
2023-03-09 13:38:45 +0900

    update main
    변수 클래스를 인터페이스로 변경

--- src/main/java/com/java/oop/Main.java
+++ src/main/java/com/java/oop/Main.java
@@ -1,12 +1,13 @@
 package com.java.oop;
 
 import com.java.oop.logic.BubbleSort;
+import com.java.oop.logic.Sort;
 
 import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        BubbleSort<String> sort = new BubbleSort<>();
+        Sort<String> sort = new BubbleSort<>();
 
         System.out.println("[result] = " + sort.sort(Arrays.asList(args)));
     }
